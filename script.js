3ae0fa71b4b14f93d6bbaf8adad14b13ca69ab26
Denis Buryachkovskiy <zekatsi5@gmail.com>
2020-04-03 20:43:22 +0300

    promise add

--- script.js
+++ script.js
@@ -2,35 +2,55 @@ var wrapper = document.getElementById('wrapper');
 
 var form = document.getElementById('filter-form');
 
-function requestCharacters(fitler, success) {
-  get(
-    'https://rickandmortyapi.com/api/character/?' + serialize(fitler),
-    function(data) {
-      var arrayOfCharacters = data.results.map(function(character) {
+function requestCharacters(fitler) {
+  return get(
+    'https://rickandmortyapi.com/api/character/?' + serialize(fitler)
+  ).then(function(data) {
+    return data.results.map(function(character) {
+      var c = new Champion(character.name, 200, 150);
+      c.setImage(character.image);
+      return c;
+    });
+  });
+}
+
+function requestCharactersWithFetch(fitler) {
+  return fetch(
+    'https://rickandmortyapi.com/api/character/?' + serialize(fitler)
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      return data.results.map(function(character) {
         var c = new Champion(character.name, 200, 150);
         c.setImage(character.image);
         return c;
       });
-
-      success(arrayOfCharacters);
-    }
-  );
+    });
 }
 
-form.elements.characterName.addEventListener(
-  'input',
-  debounce(function() {
-    requestCharacters(
-      {
-        name: this.value,
-        gender: 'male'
-      },
-      function(arrayOfCharacters) {
-        wrapper.innerHTML = '';
-        arrayOfCharacters.forEach(function(champ) {
-          champ.render(wrapper);
-        });
-      }
-    );
-  }, 200)
-);
+// form.elements.characterName.addEventListener(
+//   'input',
+//   debounce(function() {
+//     requestCharacters({
+//       name: this.value,
+//       gender: 'male'
+//     }).then(function(arrayOfCharacters) {
+//       wrapper.innerHTML = '';
+//       arrayOfCharacters.forEach(function(champ) {
+//         champ.render(wrapper);
+//       });
+//     });
+//   }, 200)
+// );
+
+requestCharacters({
+  name: 'rick',
+  gender: 'male'
+}).then(function(arrayOfCharacters) {
+  wrapper.innerHTML = '';
+  arrayOfCharacters.forEach(function(champ) {
+    champ.render(wrapper);
+  });
+});
