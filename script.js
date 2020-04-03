c4c42565c17e6393952a6045397c62a76ff142cd
Denis Buryachkovskiy <zekatsi5@gmail.com>
2020-04-03 20:58:54 +0300

    add httpclient logic

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ var wrapper = document.getElementById('wrapper');
 var form = document.getElementById('filter-form');
 
 function requestCharacters(fitler) {
-  return get(
-    'https://rickandmortyapi.com/api/character/?' + serialize(fitler)
-  ).then(function(data) {
-    return data.results.map(function(character) {
-      var c = new Champion(character.name, 200, 150);
-      c.setImage(character.image);
-      return c;
+  return get('https://rickandmortyapi.com/api/character/?' + serialize(fitler))
+    .then(transformFromXmlResponse)
+    .then(function(data) {
+      return data.results.map(function(character) {
+        var c = new Champion(character.name, 200, 150);
+        c.setImage(character.image);
+        return c;
+      });
     });
-  });
 }
 
 function requestCharactersWithFetch(fitler) {
@@ -54,3 +54,41 @@ requestCharacters({
     champ.render(wrapper);
   });
 });
+
+// requestCharactersWithFetch({
+//   name: 'rick',
+//   gender: 'male'
+// }).then(function(arrayOfCharacters) {
+//   wrapper.innerHTML = '';
+//   arrayOfCharacters.forEach(function(champ) {
+//     champ.render(wrapper);
+//   });
+// });
+
+var rickAndMortyAPI = new HttpClient('https://rickandmortyapi.com/api/');
+
+rickAndMortyAPI.handleError = function() {
+  console.log('Error');
+};
+
+rickAndMortyAPI
+  .get(
+    'character/?' +
+      serialize({
+        name: 'rick',
+        gender: 'male'
+      })
+  )
+  .then(function(data) {
+    return data.results.map(function(character) {
+      var c = new Champion(character.name, 200, 150);
+      c.setImage(character.image);
+      return c;
+    });
+  })
+  .then(function(arrayOfCharacters) {
+    wrapper.innerHTML = '';
+    arrayOfCharacters.forEach(function(champ) {
+      champ.render(wrapper);
+    });
+  });
